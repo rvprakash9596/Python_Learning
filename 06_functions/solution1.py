7924acdf853bfc85e9868c24fa966ea63c1c2567
rvprakash9596 <raviprakash9596@gmail.com>
2025-05-05 08:51:09 +0530

    square of a number

---
+++ 06_functions/solution1.py
@@ -0,0 +1,11 @@
+# Basic Function Syntax
+# Problem: Write a function to calculate and return the square of a number.
+
+def sqn(number):
+    return number ** 2
+res1 = sqn(5)
+res2 = sqn(3)
+res3 = sqn(7)
+print(f"The square of {5} = ",res1)
+print(res2)
+print(res3)
