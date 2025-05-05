8dc2fc5dab9153c414204fc87a02f45c958689e3
rvprakash9596 <raviprakash9596@gmail.com>
2025-05-05 08:58:36 +0530

    Create a function that takes two numbers as parameters and returns their sum

---
+++ 06_functions/solution2.py
@@ -0,0 +1,15 @@
+# Function with Multiple Parameters
+# Problem: Create a function that takes two numbers as parameters and returns their sum
+
+#Method 1
+def sum(a,b):
+    return a+b
+print(f"Sum of {4} + {5} =",sum(4,5))
+
+#Meyhod 2
+def sum2(a,b):
+    return a+b
+a=5
+b=10
+print(f"Sum of {a} + {b} =",sum2(a,b))
+
