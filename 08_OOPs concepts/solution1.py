3d270d0adcd5d9086cdc893a0d07d56258847083
rvprakash9596 <raviprakash9596@gmail.com>
2025-06-02 11:39:54 +0530

    Create a Car class with attributes like brand and model. Then create an instance of this class

--- 08_OOPs concepts/solution1.py
+++ 08_OOPs concepts/solution1.py
@@ -2,7 +2,7 @@
 # Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
 
 class Car:
-    def __init__(self,brand,model):
+    def __init__(self,brand,model): # init is a constructor
         # brand = None
         # model = None
         self.brand = brand
